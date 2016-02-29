de3a2585877960a62f38690b4c648e28950f69e6
lxx <listen987654321@sina.com>
2016-02-29 16:04:47 +0800

    Initial Commit

---
+++ Good/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Good
+//
+//  Created by lxx on 16/2/29.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Good/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Good
+//
+//  Created by lxx on 16/2/29.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
