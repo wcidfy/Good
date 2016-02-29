aeb6346aafc460a4797410d906059fff7ceeb25c
lxx <listen987654321@sina.com>
2016-02-29 16:18:08 +0800

    项目初始化

---
+++ Good/Classes/Main/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Good
+//
+//  Created by lxx on 16/2/29.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Good/Classes/Main/MainViewController.m
@@ -0,0 +1,24 @@
+//
+//  MainViewController.m
+//  Good
+//
+//  Created by lxx on 16/2/29.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor=[UIColor redColor];
+}
+
+
+@end
